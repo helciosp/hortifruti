2cbd88b0681cd4d574ab2d863d3f4e7e0d9b0fab
helciosp43 <helciosp43@gmail.com>
2022-01-20 10:09:16 -0300

    Criando servidor para consumir api

---
+++ Server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use("/api/fruit/all", createProxyMiddleware({
+  target: "https://www.fruityvice.com",
+  secure: false,
+  changeOrigin: true,
+})
+);
+
+app.listen(8000);
