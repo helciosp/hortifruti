c0194160026172f6effe3d538a2061b9690ff2f1
helciosp <helciosp43@gmail.com>
2023-03-30 08:03:47 -0300

    Alterando Origem

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,7 +5,7 @@ const app = express();
 
 module.exports = function() {
     app.use((req, res, next) => {
-        res.setHeader("Access-Control-Allow-Origin", "~/");
+        res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         res.header(
           "Access-Control-Allow-Headers",
           "Origin, X-Requested-With, Content-Type, Accept"
